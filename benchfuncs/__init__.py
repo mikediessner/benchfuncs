2968f7e8e284602bc19b92502b4100a0ea8e0e65
mikediessner <m.diessner2@newcastle.ac.uk>
2022-08-12 08:25:28 +0100

    added Schwefel

--- benchfuncs/__init__.py
+++ benchfuncs/__init__.py
@@ -2,3 +2,4 @@ from .sphere import Sphere
 from .hartmann import Hartmann3D, Hartmann6D
 from .ackley import Ackley
 from .griewank import Griewank
+from .schwefel import Schwefel
--- benchfuncs/schwefel.py
+++ benchfuncs/schwefel.py
@@ -1,23 +1,21 @@
-from pkgutil import iter_importers
 import numpy as np
 from benchmark_function import BenchmarkFunction
 
 
-class Griewank(BenchmarkFunction):
+class Schwefel(BenchmarkFunction):
 
     def __init__(self, dims: int, noise: float = 0.0, minimise: bool = True):
 
         self.dims = dims
-        self.bounds = np.array([[-600.0, ] * dims, [600.0, ] * dims])
-        self.optimum = {"inputs": np.array([[0.0, ] * dims]), "ouput": np.array([[0.0]])}
+        self.bounds = np.array([[-500.0, ] * dims, [500.0, ] * dims])
+        self.optimum = {"inputs": np.array([[420.9687, ] * dims]), "ouput": np.array([[0.0]])}
         self.noise = noise
         self.minimise = minimise
 
     def __call__(self, x: np.array):
         
         # compute output
-        ii = np.arange(1, self.dims+1)
-        y = np.sum(x**2/4000, axis=1) - np.prod(np.cos(x / np.sqrt(ii)), axis=1) + 1
+        y = 418.9829 * self.dims - np.sum(x * np.sin(np.sqrt(np.abs(x)), axis=1))
 
         # turn into maximisation problem
         if not self.minimise:
