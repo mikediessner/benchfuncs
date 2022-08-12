bd1746f8817d1be4df6e5a7e1b32c99d42f5f18d
mikediessner <m.diessner2@newcastle.ac.uk>
2022-08-12 08:22:15 +0100

    added Griewank

--- benchfuncs/__init__.py
+++ benchfuncs/__init__.py
@@ -1,3 +1,4 @@
 from .sphere import Sphere
 from .hartmann import Hartmann3D, Hartmann6D
 from .ackley import Ackley
+from .griewank import Griewank
---
+++ benchfuncs/griewank.py
@@ -0,0 +1,34 @@
+from pkgutil import iter_importers
+import numpy as np
+from benchmark_function import BenchmarkFunction
+
+
+class Griewank(BenchmarkFunction):
+
+    def __init__(self, dims: int, noise: float = 0.0, minimise: bool = True):
+
+        self.dims = dims
+        self.bounds = np.array([[-600.0, ] * dims, [600.0, ] * dims])
+        self.optimum = {"inputs": np.array([[0.0, ] * dims]), "ouput": np.array([[0.0]])}
+        self.noise = noise
+        self.minimise = minimise
+
+        self.a = 20
+        self.b = 0.2
+        self.c = 2*np.pi
+
+    def __call__(self, x: np.array):
+        
+        # compute output
+        ii = np.arange(1, self.dims+1)
+        y = np.sum(x**2/4000, axis=1) - np.prod(np.cos(x / np.sqrt(ii)), axis=1) + 1
+
+        # turn into maximisation problem
+        if not self.minimise:
+            y = -y
+
+        # add noise
+        noise = np.random.normal(loc=0, scale=noise, size=y.shape)
+        f = y + noise
+
+        return f.reshape((-1, 1))
